f9b57c0b6ccdb0c4433d0e51317691a6b2efd0a7
fedeviotti <fedeviotti@gmail.com>
2019-02-02 11:17:22 +0100

    modifica csrf

--- src/main/java/com/adamzareba/spring/security/oauth2/config/server/ResourceServerConfiguration.java
+++ src/main/java/com/adamzareba/spring/security/oauth2/config/server/ResourceServerConfiguration.java
@@ -23,8 +23,7 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-        	.requestMatchers()
+        http.csrf().disable().requestMatchers()
                 .antMatchers(SECURED_PATTERN).and().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SECURED_PATTERN).access(SECURED_WRITE_SCOPE)
                 .anyRequest().access(SECURED_READ_SCOPE);
