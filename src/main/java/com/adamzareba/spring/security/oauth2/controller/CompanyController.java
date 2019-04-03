ce6cbefcd4518cf0e44227276cc1ae6116abac95
fedeviotti <fedeviotti@gmail.com>
2019-04-03 23:47:26 +0200

    cors localhost

--- src/main/java/com/adamzareba/spring/security/oauth2/controller/CompanyController.java
+++ src/main/java/com/adamzareba/spring/security/oauth2/controller/CompanyController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import java.util.List;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/secured/company")
 public class CompanyController {
