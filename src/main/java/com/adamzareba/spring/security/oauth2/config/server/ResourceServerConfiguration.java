7a61ca81b0703365e621434788d4f290907b3753
fedeviotti <fedeviotti@gmail.com>
2019-04-03 23:38:30 +0200

    cors enabled for OPTIONS

--- src/main/java/com/adamzareba/spring/security/oauth2/config/server/ResourceServerConfiguration.java
+++ src/main/java/com/adamzareba/spring/security/oauth2/config/server/ResourceServerConfiguration.java
@@ -27,5 +27,6 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
                 .antMatchers(SECURED_PATTERN).and().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SECURED_PATTERN).access(SECURED_WRITE_SCOPE)
                 .anyRequest().access(SECURED_READ_SCOPE);
+        http.cors();
     }
 }
